4a6307e6dd890f2305c94a3c1e5be016d419b6b8
Sergio-Junior-Dev <85907564+Sergio-Junior-Dev@users.noreply.github.com>
2023-09-21 13:19:10 -0300

    Criação  e DOC dos métodos da classe Veiculo

--- codigo/Rota.java
+++ codigo/Rota.java
@@ -1,6 +1,7 @@
 package codigo;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 public class Rota {
     private double quilometragem;
@@ -13,6 +14,28 @@ public class Rota {
     public Rota(double quilometragem, Date data) {
         this.quilometragem = quilometragem;
         this.data = data;
+    }
+
+    /*
+     * 
+     */
+    public static int getMonthFromDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM");
+        String month = dateFormat.format(date);
+        return Integer.parseInt(month);
+    }
+
+    //Gets
+    public double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public int getMes(){
+        return getMonthFromDate(data);
     }
 
     /*
--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,6 +1,6 @@
 package codigo;
 
-import java.util.List;
+import java.util.*;
 
 public class Veiculo {
     private static final int maxRotas = 30;
@@ -12,10 +12,10 @@ public class Veiculo {
     protected double tanqueMax;
     protected double totalReabastecido;
 
-    
     /*
      * Construtor da classe Veiculo
-     * Recebe como parametro a placa do veiculo, a quantidade de litos atualmente no tanque e a capacidade máxima do tanque
+     * Recebe como parametro a placa do veiculo, a quantidade de litos atualmente no
+     * tanque e a capacidade máxima do tanque
      */
     public Veiculo(String Placa, double Tanque_Atual, double Tanque_Max) {
         this.placa = Placa;
@@ -24,51 +24,110 @@ public class Veiculo {
     }
 
     /*
-     * 
+     * Método que valida se a quantidade de rotas cadastradas para o veiculo é maior que 0 e menor que 30
+     * Não possui paramentros
+     * Retorna "true" ou "false"
      */
-    public boolean addRota(Rota rota){
-        return true;
+    public boolean validaQtdRota() {
+        if (this.qtdRotas >= 0 && this.qtdRotas <= 30) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /*
-     * 
+     * Metodo para atribuir uma rota para um veiculo
+     * Recebe uma rota como parametros
+     * Verifica se a autonomia atual é suficiente pra execultar a rota e verifica se o veiculo não atingiu o limite máximo de rotas
+     * Retorna "true" ou "false"
      */
-    public double autonomiaMaxima(){
-        return 0;
+    public boolean addRota(Rota rota) {
+        if(autonomiaAtual() >= rota.getQuilometragem() && validaQtdRota() == true){
+            rotas.add(rota);
+            qtdRotas ++;
+            return true;
+        }
+        else{
+            return false;
+
+        }
     }
 
     /*
-     * 
+     * Método que calcula a autonomia máxima do veiculo
+     * Não possui paramentros
+     * Retorna um número do tipo double
      */
-    public double autonomiaAtual(){
-        return 0;
+    public double autonomiaMaxima() {
+        return this.tanqueMax * consumo;
     }
 
     /*
-     * 
+     * Metodo para calcular a autonomia atual do veiculo
+     * Não possui paramentros
+     * Retorna um número do tipo double
      */
-    public double abastecer( double litros){
-        return 0;
+    public double autonomiaAtual() {
+        return this.tanqueAtual * consumo;
     }
 
     /*
-     * 
+     * Metodo para abastecer o veiculo
+     * Recebe como parametro a quantidade de litros do abastecimento
+     * Retorna o tanque atual: Se a quantidade de litros couber no tanque, altera a quantidade, se não retorna a mesma quantidade anterior
      */
-    public double kmNoMes(){
-        return 0;
+    public double abastecer(double litros) {
+        if(this.tanqueAtual + litros <= this.tanqueMax){
+            this.tanqueAtual = this.tanqueAtual + litros;
+            this.totalReabastecido = this.totalReabastecido + litros;
+            return tanqueAtual;
+        }
+        else{
+            return tanqueAtual;
+        }
     }
 
     /*
-     * 
+     * Metodo para calcular a quantidade total de km no mês
+     * Recebe como parametro o numero do mês
+     * Retorna a quantidade de km do mês
      */
-    public double kmTotal(){
-        return 0;
+    public double kmTotalPorMes(int mes) {
+        double kmTotal = 0;
+        for (Rota rota : rotas) {
+            // Verifique se a rota ocorreu no mês especificado
+            if (rota.getMes() == mes) {
+                kmTotal += rota.getQuilometragem();
+            }
+        }
+        return kmTotal;
     }
 
     /*
-     * 
+     * Metodo para calcular a quantidade total de km
+     * Não recebe paramentros
+     * Retorna a quantidade de km total
      */
-    public void percorrerRota( Rota rota){
-        
+    public double kmTotal() {
+        double kmTotal = 0 ;
+        for (Rota rota : rotas) {
+            kmTotal = kmTotal + rota.getQuilometragem();
+        }
+        return kmTotal();
+    }
+
+    /*
+     * Metodo para percorrer a rota
+     * Recebe a rota como parametro
+     * Diminui a quantidade de litros do tanque conforme a distancia percorrida
+     */
+    public void percorrerRota(Rota rota) {
+        for (Rota r : rotas) {
+            if( rota == r)
+            {
+                this.tanqueAtual = tanqueAtual-(rota.getQuilometragem()/consumo);
+            }
+        }
     }
 }
